6522104bba1992f2e105d529351abf3989bcc219
Haikerwal <abhishek.techeunoia@gmail.com>
2024-11-15 09:03:16 +0530

    attempting to fix path 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {Route, Routes, HashRouter as Router} from "react-router-dom";
+import { Route, Routes, HashRouter as Router } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Navbar from "./components/Navbar.jsx";
 import AddBook from "./pages/AddBook.jsx";
@@ -7,28 +7,25 @@ import Books from "./pages/Books.jsx";
 import NotFound404 from "./pages/NotFound404.jsx";
 import MobileNavbar from "./components/MobileNavbar.jsx";
 import { Provider } from 'react-redux';
-import {store} from "./redux/store.js"
-
-
+import { store } from "./redux/store.js";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Router>
-        <Navbar/>
-        <MobileNavbar/>
+      <Router basename="/libraryManagementSystem">
+        <Navbar />
+        <MobileNavbar />
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/libraryManagementSystem/" element={<Home/>}/>
-          <Route path="/browseBooks" element={<Books/>}/>
-          <Route path="/addBook" element={<AddBook/>}/>
-          <Route path="/bookdetails/:id" element={<BookDetails/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/browseBooks" element={<Books />} />
+          <Route path="/addBook" element={<AddBook />} />
+          <Route path="/bookdetails/:id" element={<BookDetails />} />
           <Route path="/404" element={<NotFound404 />} />
           <Route path="*" element={<NotFound404 />} />
         </Routes>
       </Router>
     </Provider>
-  )
+  );
 }
 
-export default App
+export default App;
